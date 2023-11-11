7125239653d142d5903f9975a752cbad1b1e791b
Alberto Rico <alrico88@gmail.com>
2023-11-11 18:38:48 +0100

    fix: informative error message

--- app/metar/[icao]/error.tsx
+++ app/metar/[icao]/error.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-export default function Error({
-  error,
-}: {
+export default function Error(_props: {
   error: Error & { digest?: string };
   reset: () => void;
 }) {
@@ -10,7 +8,9 @@ export default function Error({
     <div className="container">
       <div className="row">
         <div className="col">
-          <div className="alert alert-danger mb-0">{error.message}</div>
+          <div className="alert alert-danger mb-0">
+            Airport or METAR not found
+          </div>
         </div>
       </div>
     </div>
